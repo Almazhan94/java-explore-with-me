d74f938ec647f4559bf3c7e3ac87c920b9386b73
Almaz <almazhan94@yandex.ru>
2023-07-31 23:08:08 +0300

    Диплом. Этап 3. Проверка 1.

--- main-service/src/main/java/ru/practicum/main/service/comment/CommentController.java
+++ main-service/src/main/java/ru/practicum/main/service/comment/CommentController.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.main.service.request.dto.RequestDto;
-import ru.practicum.main.service.request.dto.RequestStatusUpdateDto;
-import ru.practicum.main.service.request.dto.RequestStatusUpdateResultDto;
 
 import java.util.List;
 
@@ -37,7 +34,7 @@ public class CommentController {
         log.info("Пользователь с userId = {} обновляет комментарий с commentId = {} " +
             "для события с eventId = {} requestStatusUpdateDto = {}", userId, commentId, eventId, updateCommentDto);
 
-        return commentService.patchByUser( userId, commentId, eventId, updateCommentDto);
+        return commentService.patchByUser(userId, commentId, eventId, updateCommentDto);
     }
 
     @GetMapping("/users/{userId}/comment")
